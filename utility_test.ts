ea45ac716d675143d8aae828c9086c706654487d
R. S. Doiel <rsdoiel@gmail>
2025-05-05 11:21:14 -0700

    added coverage report

--- utility_test.ts
+++ utility_test.ts
@@ -1,15 +1,28 @@
-import { assertEquals } from "@std/assert";
-import {verifyIdentifier } from "./utility.ts";
-
-const verified_ids: string[] = [
-    "https://caltechlibrary.github.io/dataset/README.md"
-];
+import { assertEquals, assertNotEquals } from "@std/assert";
+import { verifyIdentifier, getObject } from "./utility.ts";
 
 function noOp(s: string): boolean {
     return true;
 }
 
-for (let id of verified_ids) {
-    let res = await verifyIdentifier(id, id, noOp);
-    assertEquals(res, true);
-}
+Deno.test('test verifyIdentifier', async function() {
+    const object_urls: string[] = [
+        "https://caltechlibrary.github.io/dataset/README.md",
+    ];
+        
+    for (let url of object_urls) {
+        let res = await verifyIdentifier(url, url, noOp);
+        assertEquals(res, true);
+    }
+});
+
+Deno.test("test getObject", async function() {
+    const object_urls: string [] = [
+        "https://feeds.library.caltech.edu/recent/article.json",
+    ];
+    for (let url of object_urls) {
+        let res = await getObject(url, url, noOp);
+        //console.log(`DEBUG res -> ${res}`);
+        assertNotEquals(res, undefined);
+    }
+});
